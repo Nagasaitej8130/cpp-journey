d36608a21c3d283b05a16af6b876b4da14b4950f
Naga Sai Teja Bollimuntha <bollimunthanagasaiteja@gmail.com>
2026-01-02 19:02:33 +0530

    Create a greeting program in C++
    Implement a program that greets a user by name.

---
+++ AUT_Cpp_1Jan2026.cpp
@@ -0,0 +1,18 @@
+/*ASSUIT UNIVERSITY TRAINING SHEET 1 Problem1 
+Given a name S. Print "Hello, (name)" without parentheses.
+Input
+Only one line containing a string S.
+Output
+Print "Hello, " without quotes, then print name.*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+string S;
+cout<<"Enter a name: ";
+cin>>S;
+cout<<"Hello,"<<S<<endl;
+return 0;
+
+}0
